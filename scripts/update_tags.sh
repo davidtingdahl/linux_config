30142e07c95f200e7b07d211554806f9952fa073
David Tingdahl <david.tingdahl@gmail.com>
2018-09-09 17:51:11 +0200

    update tags

---
+++ scripts/update_tags.sh
@@ -0,0 +1,11 @@
+#/usr/bin/bash
+
+
+git_dir=$(git rev-parse --show-toplevel)
+
+[[ ! -d "$git_dir" ]] && echo "Not a git repo" && exit 1
+
+(
+    cd "$git_dir"/visual_odometry/cpp
+    find -type f -iname "*.h"  -or -iname "*.cc" | etags -
+)
