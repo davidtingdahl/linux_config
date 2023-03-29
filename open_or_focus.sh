c2b8e2211de4e5e9652dfea448e321b2f4bec8f8
David Tingdahl <david.tingdahl@zenseact.com>
2023-03-29 15:04:41 +0200

    Simplified open-or-focus script

--- open_or_focus.sh
+++ open_or_focus.sh
@@ -1,37 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
-function open_or_focus
-{
-    label=$1
-    command=$2
-
-    wid=$(xdotool search --onlyvisible "$label"| head -n1)
-    echo $wid
-
-    if [ -z $wid ]
-    then $command
-    else
-        xdotool windowactivate --sync $wid
-    fi
-
-}
-
-[[ $# != 1 ]] && echo "Give command as argument" && exit 1
-
-set -ex
-
+# If the given program is running, it will receive focus. Otherwise it will open 
+if [ $# -ne 1 ]
+then
+    echo "Give exec to open-or-focus as argument"
+    exit 1
+fi
 cmd=$1
-
-[[ $cmd == "emacs" ]]    && open_or_focus "GNU Emacs" emacs
-[[ $cmd == "terminal" ]] && open_or_focus "Terminal" xfce4-terminal
-[[ $cmd == "slack" ]]    && open_or_focus "slack" slack
-[[ $cmd == "teams" ]]    && open_or_focus "teams" teams
-[[ $cmd == "firefox" ]]  && open_or_focus "Mozilla" firefox
-
-[[ $cmd == "calendar" ]] && browse https://outlook.office.com/calendar/view/month
-[[ $cmd == "mail" ]] && browse https://outlook.office.com/mail/
-[[ $cmd == "gerrit" ]] && browse https://gerrit.cicd.autoheim.net/dashboard/self
-
-[[ $cmd == "spotify" ]] && open_or_focus Spotify spotify
-
+set -ex
+xdotool search --onlyvisible --class $1 windowactivate --sync || $cmd
